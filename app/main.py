25ee8855763db5dd0e44227cac04df2d992b66e4
sevan-garo <sevangarois@mac.home>
2026-09-09 15:34:39 +0200

    application starters

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="You Are Routered",
+    version="0.1.0",
+)
+
+
+@app.get("/health")
+def health() -> dict[str, str]:
+    return {"status": "ok"}
